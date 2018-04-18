204bae6de3ed2eacf5516c49a1ada8eef196ab61
hoxtygen <hoxtygen@live.com>
2018-04-18 23:20:41 +0100

    added sass and css files

---
+++ js/app.js
@@ -0,0 +1,12 @@
+//Get all the keys from the document
+var keys = document.querySelectorAll('#calculator span');
+//console.log(keys);
+//Add onclick event to all the keys and perform operations
+for (let i = 0; i < keys.length; i++) {
+    keys[i].onclick= function (event) {
+        //console.log(keys[i].innerHTML);
+        //Get the input and button values
+        var input = document.querySelectorAll("#display");
+    }
+    
+}
